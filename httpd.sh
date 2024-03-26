17a25432649dadbc6bbfe41b4c11222f9362a44d
balaji2072 <sulagambalaji998@gmail.com>
2024-03-26 17:29:07 +0530

    new

--- httpd.sh
+++ httpd.sh
@@ -4,7 +4,6 @@ sudo systemctl stop nginx
 sudo systemctl disable nginx
 sudo yum install httpd -y
 sudo systemctl start httpd
-
 sudo systemctl enable httpd
- sudo systemctl status httod
+sudo systemctl status httod
  `
